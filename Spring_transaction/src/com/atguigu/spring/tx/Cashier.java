7d2e635a6352610501d74c547f85951131033635
pl1111w <1362596047@qq.com>
2019-04-13 16:33:16 +0800

    二分查找

--- Spring_transaction/src/com/atguigu/spring/tx/Cashier.java
+++
@@ -1,8 +0,0 @@
-package com.atguigu.spring.tx;
-
-import java.util.List;
-
-public interface Cashier {
-
-	public void checkout(String name,List<String> istns);
-}
--- Spring_transaction/src/com/atguigu/spring/tx_xml/Cashier.java
+++
@@ -1,8 +0,0 @@
-package com.atguigu.spring.tx_xml;
-
-import java.util.List;
-
-public interface Cashier {
-
-	public void checkout(String name,List<String> istns);
-}
